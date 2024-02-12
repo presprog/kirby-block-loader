ba07babc41b6f8249960537293839a3e80c59cf3
Benedict Massolle <bm@presentprogressive.de>
2024-02-12 21:43:12 +0100

    Include vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'presprog/kirby-block-loader',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6e13dec504c8669167208569a2142ddfe6cdacae',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'presprog/kirby-block-loader' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6e13dec504c8669167208569a2142ddfe6cdacae',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
